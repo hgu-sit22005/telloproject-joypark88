f35745dd085b2e18aaf922298a141703ddc92716
joypark88 <21700253@handong.edu>
2019-11-26 15:19:10 +0900

    drone practice1

--- bin/tellopro.py
+++ bin/tellopro.py
@@ -7,8 +7,14 @@ import TelloPro
 tello = Tello()
 
 command_lst = []
-command_lst.append(TelloPro.get_instance('takeoff'))
-command_lst.append(TelloPro.get_instance('land'))
+command_lst.append(TelloPro.get_instance('takeoff', -1))
+
+command_lst.append(TelloPro.get_instance('up', 20))
+
+for i in range(4):
+	command_lst.append(TelloPro.get_instance('flip', i))
+
+command_lst.append(TelloPro.get_instance('land', -1))
 
 for command in command_lst:
 	tello.send_command_instance(command)
---
+++ build/CMakeFiles/TelloPro.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/telloproject-joypark88/src/flip.cpp" "/data/telloproject-joypark88/build/CMakeFiles/TelloPro.dir/src/flip.cpp.o"
+  "/data/telloproject-joypark88/src/land.cpp" "/data/telloproject-joypark88/build/CMakeFiles/TelloPro.dir/src/land.cpp.o"
+  "/data/telloproject-joypark88/src/python_interface.cpp" "/data/telloproject-joypark88/build/CMakeFiles/TelloPro.dir/src/python_interface.cpp.o"
+  "/data/telloproject-joypark88/src/takeoff.cpp" "/data/telloproject-joypark88/build/CMakeFiles/TelloPro.dir/src/takeoff.cpp.o"
+  "/data/telloproject-joypark88/src/up.cpp" "/data/telloproject-joypark88/build/CMakeFiles/TelloPro.dir/src/up.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.6m"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
